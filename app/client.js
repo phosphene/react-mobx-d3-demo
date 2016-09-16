0ff3a9b283b5724ae722ed096a688f7bb558f1d9
ed phillips <ed@cronos.net>
2016-09-16 13:54:48 -0700

    add dc.css to client.js

--- app/client.js
+++ app/client.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'dc/dc.css';
 
 const rootEl = document.getElementById('root');
 
