bf85adb3dcc14b1cd0f9d8c335eb24476ca77213
ed phillips <ed@cronos.net>
2016-09-20 19:43:12 -0700

    add nav to header

--- app/shared/components/Header/index.js
+++ app/shared/components/Header/index.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
-//import styles from './styles.css';
+//import { Button } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+
 
 export default function Header() {
   return (
-    <header role="banner">
-      <div>
-        <nav>
-          <ul>
-            <li><IndexLink to="/">Home</IndexLink></li>
-            <li><Link to="bubble">Cluster Bubble</Link></li>
-            <li><Link to="v4port">V4</Link></li>
-            <li><Link to="thrashdash">Thrash Dash</Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+       <div>
+       <Navbar>
+         <Nav bsStyle="tabs">
+            <NavItem href="/">Home</NavItem>
+            <NavItem href="thrashdash">Thrash Dash</NavItem>
+            <NavItem href="dreamdash">Dream Dash</NavItem>
+        </Nav>
+       </Navbar>
+       </div>
   );
 }
