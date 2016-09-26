7b408122ecef3b84eee862b6edd75a1e2f63e8d4
ed phillips <ed@cronos.net>
2016-09-26 11:11:42 -0700

    add themeswitcher

--- app/modules/NOAADash/index.js
+++ app/modules/NOAADash/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NOAAWrapper from './components/NOAAWrapper';
+import { ThemeSwitcher} from 'react-bootstrap-theme-switcher';
 /* This file can serve as the index for this level of component namespace
  * it is currently being used that way as a convenient way to develop
  * It is here as a design choice*/
@@ -11,8 +12,13 @@ export default class NOAADash extends Component {
   }
 
   render() {
+    let themes = ['default', 'cyborg', 'darkly'];
     return (
+      <ThemeSwitcher themePath="/themes" defaultTheme="cyborg" storeThemeKey="thrashtheme" themes={themes}>
+
      <NOAAWrapper/>
+
+      </ThemeSwitcher>
     );
   }
 }
